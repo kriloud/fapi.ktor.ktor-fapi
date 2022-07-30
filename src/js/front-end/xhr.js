d90cf7a8d30f00ffea0e5bc53007f3bc5637b41a
0finderfail <ilyalvov2007@gmail.com>
2022-07-30 13:03:28 +0300

    add front-end

--- src/js/front-end/xhr.js
+++ src/js/front-end/xhr.js
@@ -1,15 +1,7 @@
 const requestURL = 'https://jsonplaceholder.typicode.com/todos/1'
 const xhr = new XMLHttpRequest()
-const test = 'my first JavaScript 0_o script?'
-const x1 = 10
-const x2 = 20
-const x3 = x2 * x1
-console.log(x1)
-console.log(x2)
-console.log(x3)
 xhr.open('GET', requestURL)
 xhr.onload = () => {
   console.log(xhr.response)
 }
-console.log(test)
 xhr.send()
